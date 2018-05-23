0bd106db5a0208f2b9c5e28c53b7772f78cf0474
Pavel Vishnyakov <djhumster@gmail.com>
2018-05-23 23:13:25 +0300

    ufw блок по списку

---
+++ ufw_block.py
@@ -0,0 +1,16 @@
+import  re, subprocess, sys
+
+if len(sys.argv) != 2:
+    exit()
+
+in_file = sys.argv[1]
+
+with open(in_file, 'r') as f:
+    reader = f.read()
+
+networks = re.findall('(?:\d{1,3}\.){3}\d{1,3}\/\d{2}', reader)
+
+for net in networks:
+    p = subprocess.Popen(["ufw", "deny", "from", net], stdout=subprocess.PIPE)
+    output, err = p.communicate()
+    print  (output)
