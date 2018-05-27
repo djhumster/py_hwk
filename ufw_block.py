1092d6b9e48a90d7976cad17bd323f5a4af7748b
Pavel Vishnyakov <djhumster@gmail.com>
2018-05-27 20:04:43 +0300

    fix in findall

--- ufw_block.py
+++ ufw_block.py
@@ -8,7 +8,7 @@ in_file = sys.argv[1]
 with open(in_file, 'r') as f:
     reader = f.read()
 
-networks = re.findall('(?:\d{1,3}\.){3}\d{1,3}\/\d{2}', reader)
+networks = re.findall(r'(?:\d{1,3}\.){3}\d{1,3}\/\d{2}', reader)
 
 for net in networks:
     p = subprocess.Popen(["ufw", "deny", "from", net], stdout=subprocess.PIPE)
